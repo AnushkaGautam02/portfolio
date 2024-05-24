102bc430a297e9e6474210d48148da9425323202
AnushkaGautam02 <anushkagautam0213@gmail.com>
2024-05-24 23:05:58 +0530

    portfolio

---
+++ src/utils/data.js
@@ -0,0 +1,72 @@
+export const SKILLS =[
+    {
+        title: "Frontend",
+        icon:'../../assets/images/tab.png',
+        skills:[
+            { skill: "HTML5", percentage:"90%"},
+            {skill:"CSS3", percentage:"80%"},
+            {skill:"JavaScript", percentage:"75%"},
+            {skill:"React.js", percentage:"60%"},
+        ],
+    },
+    {
+        title: "Backend",
+        icon:'../../assets/images/backend.png',
+        skills:[
+            {skill:"Node.js", percentage:"65%"},
+            {skill:"Express.js", percentage:"65%"},
+            {skill:"PHP", percentage:"70%"},
+        ],
+    },
+    {
+        title:"Tools",
+        icon:"../../assets/images/tools.png",
+        skills:[
+            {skill:"Git & GitHub", percentage:"70%"},
+            {skill:"Visual Studio Code", percentage:"75%"},
+            {skill:"Responsive Design", percentage:"70%"},
+        ],
+    },
+    {
+        title:"Soft Skills",
+        icon:'../../assets/images/icons8-communication-64.png',
+        skills:[
+            {skill:"Problem-solving", percentage:"80%"},
+            {skill:"Collaboration", percentage:"85%"},
+            {skill:"Attention to Details", percentage:"75%"},
+        ],
+    },
+];
+
+export const WORK_EXPERIENCE = [
+    {
+        title: "Junior Software Engineer at Infoseek Software Solutions",
+        date: "July 2022 - Present ",
+        responsibilities:[
+            "Developed and maintained client website, ensuring high-quality standards and timely project delivery.",
+            "Implemented SEO best practices, leading to a 25% increase in organic search traffic for key client.",
+            "worked closely with the design team to create pixel-perfect, cross-browser-compatible web pages.",
+            "Conducted  code reviews and mentored junior developers in adopting best coding practices.",
+        ],
+    },
+    {
+        title:"Frontend Developer at Infoseek Software Solutions",
+        date:" Feb 2023 - Present",
+        responsibilities:[
+            "Developed and maintained client website, ensuring high-quality standards and timely project delivery.",
+            "Implement Frontend websites using wordpress and React.js",
+            "Conducted  code reviews and mentored junior developers in adopting best coding practices.",
+            "Use Different frameworks for styling like: - CSS, Bootstrap, Tailwind CSS"
+        ],
+    },
+    {
+        title:"Software Engineer Trainee",
+        date:"Feb-2022 - June 2022",
+        responsibilities:[
+            "Assisted in the development of a new e-commerce plateform, contributing to the implementation of frontend features.",
+            "Conducted performance optimiaztion tasks, resulting in a 15% improvement in page times.",
+            "Participated in daily stand-up meetings and collaborated with senior developers to troubleshoot and resolve them.",
+            "Gained hands-on exprience with version control system, specifically Git, in a collaborative development efficiently."
+        ]
+    }
+]
